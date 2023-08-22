7cbaa96c8a99f799495b9ac076023c5c51f7ee08
Chunwei Xia <120237776@qq.com>
2023-08-22 14:58:37 +0000

    Add query key softmax limited blocks

--- src/operator_fusion/gpt-2/kernels/gemm.cu
+++ src/operator_fusion/gpt-2/kernels/gemm.cu
@@ -2,6 +2,7 @@
 #include <cuda/pipeline>
 #include <mma.h>
 
+#include "../../../cuda_kernel_utils.h"
 // #include "../gpt2-large.h"
 // #include "fused_sqq_bert.cu"
 #include "gemm_three_stages.h"
@@ -858,6 +859,240 @@ __global__ void gemm_k2_limited_blocks(const half *__restrict__ matrix_a,
         }
     }
 
+    __syncthreads();
+    // batch_id*384*384 + row_block_id * 8 * 16 + xxx * 384
+#pragma unroll
+    for (int i = 0; i < kBlockColTiles * kWmmaN / kStoreColsPerIter; ++i) {
+        *reinterpret_cast<float4 *>(
+            matrix_c + batched_id * kSeqLength * kSeqLength +
+            row_block_id * kBlockRowTiles * kWmmaM +
+            (col_block_id * kBlockColTiles * kWmmaN + i * kStoreColsPerIter +
+             threadIdx.x / kStoreLanesPerRow) *
+                kSeqLength +
+            (threadIdx.x & (kStoreLanesPerRow - 1)) * sizeof(float4) /
+                sizeof(half)) =
+            *reinterpret_cast<float4 *>(
+                acc_shared +
+                (i * kStoreColsPerIter + threadIdx.x / kStoreLanesPerRow) *
+                    (kBlockRowTiles * kWmmaM + kAccSkew) +
+                (threadIdx.x & (kStoreLanesPerRow - 1)) * sizeof(float4) /
+                    sizeof(half));
+    }
+    }// end of batch tile
+}
+
+
+
+__global__ void gemm_k2_limited_blocks_div_softmax(const half *__restrict__ matrix_a,
+                        const half *__restrict__ matrix_b,
+                        float* softmax_sum, half *__restrict__ matrix_c) {
+    using namespace nvcuda;
+    using namespace souffle::gpt2::AttnQueryKeyParamsLimitedBlocks;
+    enum {
+        kBlockRowTiles = kBlockRowWarps * kGemmK2WarpRowTiles,
+        kBlockColTiles = kBlockColWarps * kGemmK2WarpColTiles,
+    };
+
+    extern __shared__ half all_shared_mem[];
+
+    half *matrix_a_shared = all_shared_mem;
+
+    half *matrix_b_shared =
+        matrix_a_shared + kBlockRowTiles * kWmmaM * (kHeadSize + kInputSkew);
+
+    half *acc_shared = all_shared_mem;
+
+    nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, kWmmaM, kWmmaN, kWmmaK, half,
+                           nvcuda::wmma::row_major>
+        wmma_matrix_a[kGemmK2WarpRowTiles];
+    nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, kWmmaM, kWmmaN, kWmmaK, half,
+                           nvcuda::wmma::col_major>
+        wmma_matrix_b[kGemmK2WarpColTiles];
+    nvcuda::wmma::fragment<nvcuda::wmma::accumulator, kWmmaM, kWmmaN, kWmmaK,
+                           half>
+        wmma_accumulator[kGemmK2WarpColTiles * kGemmK2WarpRowTiles];
+
+    const int row_warp_id = (threadIdx.x / kWarpSize) % kBlockRowWarps;
+    const int col_warp_id = (threadIdx.x / kWarpSize) / kBlockRowWarps;
+    const int batch_stride = (kSeqLength / kBlockColTiles / kWmmaN) *
+                             (kSeqLength / kBlockRowTiles / kWmmaM);
+    for(int bt=0; bt<kBatchTiles; bt++){
+    // original num_blocks = (kSeqLength / kBlockRowTiles / kWmmaM) * (kSeqLength / kBlockColTiles / kWmmaN) * kNumHead
+    // Now num_blocks = (kSeqLength / kBlockRowTiles / kWmmaM) * (kSeqLength / kBlockColTiles / kWmmaN) * kNumHead / kBatchTile
+    // blockIdx.x = 90, 
+    const int batched_id =  bt * (kGridBlocks / batch_stride) + blockIdx.x / batch_stride; // 2*10+[0,10]
+    const int row_block_id =
+        blockIdx.x % batch_stride % (kSeqLength / kBlockRowTiles / kWmmaM); // From 0 to 3
+    const int col_block_id =
+        blockIdx.x % batch_stride / (kSeqLength / kBlockRowTiles / kWmmaM); // From 0 to 3
+
+#pragma unroll
+    for (int col = 0; col < kGemmK2WarpColTiles; ++col) {
+#pragma unroll
+        for (int row = 0; row < kGemmK2WarpRowTiles; ++row) {
+            nvcuda::wmma::fill_fragment(
+                wmma_accumulator[col * kGemmK2WarpRowTiles + row], 0.0f);
+        }
+    }
+
+    enum {
+        kThreads = kBlockRowWarps * kBlockColWarps * kWarpSize,
+        kLoadLanesPerRow = kHeadSize / (sizeof(float4) / sizeof(half)),
+        kLoadColsPerIter = kThreads / kLoadLanesPerRow,
+
+        kStoreLanesPerRow =
+            kWmmaM * kBlockRowTiles / (sizeof(float4) / sizeof(half)),
+        kStoreColsPerIter = kThreads / kStoreLanesPerRow,
+    };
+
+    cuda::pipeline<cuda::thread_scope_thread> pipe = cuda::make_pipeline();
+    const auto shape = cuda::aligned_size_t<alignof(float4)>(sizeof(float4));
+
+    pipe.producer_acquire();
+#pragma unroll
+    for (int i = 0; i < kBlockRowTiles * kWmmaM / kLoadColsPerIter; ++i) {
+        // matrix_a_shared shape: (8*16, 64+8)
+        cuda::memcpy_async(
+            reinterpret_cast<float4 *>(
+                matrix_a_shared +
+                (i * kLoadColsPerIter + threadIdx.x / kLoadLanesPerRow) *
+                    (kHeadSize + kInputSkew) +
+                (threadIdx.x & (kLoadLanesPerRow - 1)) * sizeof(float4) /
+                    sizeof(half)),
+            // a shape: (12, 384, 64)
+            reinterpret_cast<const float4 *>(
+                matrix_a + batched_id * kSeqLength * kHeadSize +
+                (row_block_id * kBlockRowTiles * kWmmaM + i * kLoadColsPerIter +
+                 threadIdx.x / kLoadLanesPerRow) *
+                    kHeadSize +
+                (threadIdx.x & (kLoadLanesPerRow - 1)) *
+                    (sizeof(float4) / sizeof(half))),
+            shape, pipe);
+    }
+
+#pragma unroll
+    for (int i = 0; i < kBlockColTiles * kWmmaN / kLoadColsPerIter; ++i) {
+        // matrix_b_shared shape: (8*16, 64+8)
+        cuda::memcpy_async(
+            reinterpret_cast<float4 *>(
+                matrix_b_shared +
+                (i * kLoadColsPerIter + threadIdx.x / kLoadLanesPerRow) *
+                    (kHeadSize + kInputSkew) +
+                (threadIdx.x & (kLoadLanesPerRow - 1)) * sizeof(float4) /
+                    sizeof(half)),
+            // a shape: (12, 384, 64)
+            reinterpret_cast<const float4 *>(
+                matrix_b + batched_id * kSeqLength * kHeadSize +
+                (col_block_id * kBlockColTiles * kWmmaN + i * kLoadColsPerIter +
+                 threadIdx.x / kLoadLanesPerRow) *
+                    kHeadSize +
+                (threadIdx.x & (kLoadLanesPerRow - 1)) *
+                    (sizeof(float4) / sizeof(half))),
+            shape, pipe);
+    }
+    pipe.producer_commit();
+    pipe.consumer_wait();
+    __syncthreads();
+
+#pragma unroll
+    for (int tile_k = 0; tile_k < kHeadSize / kWmmaK; ++tile_k) {
+#pragma unroll
+        for (int tile_m = 0; tile_m < kGemmK2WarpRowTiles; ++tile_m) {
+            nvcuda::wmma::load_matrix_sync(
+                wmma_matrix_a[tile_m],
+                (matrix_a_shared +
+                 (row_warp_id * kGemmK2WarpRowTiles + tile_m) * kWmmaM *
+                     (kHeadSize + kInputSkew) +
+                 tile_k * kWmmaK),
+                kHeadSize + kInputSkew);
+        }
+#pragma unroll
+        for (int tile_n = 0; tile_n < kGemmK2WarpColTiles; ++tile_n) {
+            nvcuda::wmma::load_matrix_sync(
+                wmma_matrix_b[tile_n],
+                (matrix_b_shared +
+                 (col_warp_id * kGemmK2WarpColTiles + tile_n) * kWmmaN *
+                     (kHeadSize + kInputSkew) +
+                 tile_k * kWmmaK),
+                kHeadSize + kInputSkew);
+        }
+#pragma unroll
+        for (int tile_m = 0; tile_m < kGemmK2WarpRowTiles; ++tile_m) {
+#pragma unroll
+            for (int tile_n = 0; tile_n < kGemmK2WarpColTiles; ++tile_n) {
+                nvcuda::wmma::mma_sync(
+                    wmma_accumulator[tile_m + tile_n * kGemmK2WarpRowTiles],
+                    wmma_matrix_a[tile_m], wmma_matrix_b[tile_n],
+                    wmma_accumulator[tile_m + tile_n * kGemmK2WarpRowTiles]);
+            }
+        }
+    }
+    pipe.consumer_release();
+    __syncthreads();
+
+#pragma unroll
+    for (int tile_n = 0; tile_n < kGemmK2WarpColTiles; ++tile_n) {
+#pragma unroll
+        for (int tile_m = 0; tile_m < kGemmK2WarpRowTiles; ++tile_m) {
+            nvcuda::wmma::store_matrix_sync(
+                acc_shared +
+                    (col_warp_id * kGemmK2WarpColTiles + tile_n) * kWmmaK *
+                        (kBlockRowTiles * kWmmaM + kAccSkew) +
+                    (row_warp_id * kGemmK2WarpRowTiles + tile_m) * kWmmaM,
+                wmma_accumulator[tile_n * kGemmK2WarpRowTiles + tile_m],
+                (kBlockRowTiles * kWmmaM + kAccSkew),
+                nvcuda::wmma::mem_col_major);
+        }
+    }
+
+    __syncthreads();
+
+    // Do div and softmax
+    // Each warp compute one row
+    cooperative_groups::grid_group grid = cooperative_groups::this_grid();
+    {
+        const int laneIdx = threadIdx.x % kWarpSize;
+        const int warpIdx = threadIdx.x >> 5; // threadIdx.x / 32
+        const int warpNum = blockDim.x >> 5; // blockIdx.x /128
+        const int vecLength = sizeof(half2) / sizeof(half);
+        half2 local_sum(0.0, 0.0);
+        half2 factor = half2(1.0, 1.0) / half2(sqrtf(1280), sqrtf(1280));
+        // x[i] = exp(x[i] / sqrt(d_model))
+        #pragma unroll
+        for(int i=0; i< kBlockColTiles * kWmmaN / warpNum; ++i){
+            const int shared_row = (i * warpNum + warpIdx);
+            #pragma unroll
+            for(int j=0; j<kBlockRowTiles * kWmmaM; j += (warpSize * vecLength)){
+                const int shared_col = j + laneIdx * vecLength;
+                auto tmp = *(half2*)(acc_shared + shared_row * (kBlockRowTiles * kWmmaM + kAccSkew) + shared_col);
+                tmp = tmp * factor;
+                tmp = half2(__expf(__half2float(tmp.x)), __expf(__half2float(tmp.y)));
+                *(half2*)(acc_shared + shared_row * (kBlockRowTiles * kWmmaM + kAccSkew) + shared_col) = tmp;
+                local_sum += tmp;
+            }
+            half sum = local_sum.x + local_sum.y;
+            sum = warpReduceSum(sum);
+            if(laneIdx == 0){
+                const int global_row = batched_id * kSeqLength + col_block_id * kBlockColTiles + shared_row;
+                atomicAdd(softmax_sum + global_row , __half2float(sum));
+            }
+        }
+        grid.sync();
+        // x[i] = x[i] / sum
+        for(int i=0; i< kBlockColTiles * kWmmaN / warpNum; ++i){
+            #pragma unroll
+            const int shared_row = (i * warpNum + warpIdx);
+            const int global_row = batched_id * kSeqLength + col_block_id * kBlockColTiles + shared_row;
+            half exp_sum = __float2half(softmax_sum[global_row]);
+            #pragma unroll
+            for(int j=0; j<kBlockRowTiles * kWmmaM; j += (warpSize * vecLength)){
+                const int shared_col = j + laneIdx * vecLength;
+                auto tmp = *(half2*)(acc_shared + shared_row * (kBlockRowTiles * kWmmaM + kAccSkew) + shared_col);
+                tmp = tmp / half2(exp_sum, exp_sum);
+                *(half2*)(acc_shared + shared_row * (kBlockRowTiles * kWmmaM + kAccSkew) + shared_col) = tmp;
+            }
+        }
+    }
     __syncthreads();
     // batch_id*384*384 + row_block_id * 8 * 16 + xxx * 384
 #pragma unroll
